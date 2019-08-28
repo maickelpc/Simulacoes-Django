8f2f8722dfcb6db494ab93b4a3a87e442e767273
Maickel <maickelpc@gmail.com>
2019-08-28 13:34:00 -0300

    Commit projeto

--- simulacoes/core/admin.py
+++ simulacoes/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import  Acelerometro
 
 # Register your models here.
+class AcelerometroAdmin(admin.ModelAdmin):
+    search_fields = ['codigo','descricao','localizacao']
+    list_display = ['id','codigo','descricao','localizacao','eixoX','eixoY','eixoZ']
+    list_editable = ['eixoX','eixoY','eixoZ']
+    ordering = ['codigo']
+
+admin.site.register(Acelerometro, AcelerometroAdmin)
--- simulacoes/core/models.py
+++ simulacoes/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Acelerometro(models.Model):
+    id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=100, unique=True, verbose_name='Código')
+    descricao = models.CharField(max_length=100, verbose_name='Descrição')
+    localizacao = models.CharField(max_length=100,verbose_name='Localização')
+
+    eixoX = models.BooleanField(verbose_name='Eixo X')
+    eixoY = models.BooleanField(verbose_name='Eixo Y')
+    eixoZ = models.BooleanField(verbose_name='Eixo Z')
+
+
+    def __str__(self):
+        return self.descricao;
+
+    class Meta:
+        verbose_name = 'Acelerômetro'
+        verbose_name_plural = 'Acelerômetros'
+
+
+# Criar a classe responsável pelo arquivo.
