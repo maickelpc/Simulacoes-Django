ac49238462415d6ce96041d1146c445b4708d5be
Gabriel <gulissessti@hotmail.com>
2019-08-28 23:40:27 -0300

    comitando alterações

--- simulacoes/core/admin.py
+++ simulacoes/core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import  Acelerometro
+from .models import  Acelerometro,Arquivo
 
 # Register your models here.
+class ArquivoAdmin(admin.ModelAdmin):
+    search_fields = ['codigo','dataLeitura']
+    list_display = ['codigo','canais','dataLeitura','dataUpload']
+    ordering = ['dataUpload','codigo']
+
 class AcelerometroAdmin(admin.ModelAdmin):
     search_fields = ['codigo','descricao','localizacao']
     list_display = ['id','codigo','descricao','localizacao','eixoX','eixoY','eixoZ']
@@ -9,3 +14,4 @@ class AcelerometroAdmin(admin.ModelAdmin):
     ordering = ['codigo']
 
 admin.site.register(Acelerometro, AcelerometroAdmin)
+admin.site.register(Arquivo, ArquivoAdmin)
--- simulacoes/core/models.py
+++ simulacoes/core/models.py
@@ -19,5 +19,20 @@ class Acelerometro(models.Model):
         verbose_name = 'Acelerômetro'
         verbose_name_plural = 'Acelerômetros'
 
+class Arquivo(models.Model):
+    id = models.AutoField(primary_key=True)
+    codigo = models.CharField(max_length=100,unique=True,verbose_name='Código')
+    documento = models.FileField(verbose_name='Arquivo')
+    canais = models.IntegerField(verbose_name='Canais')
+    dataLeitura = models.DateField(verbose_name='Data de Leituras') 
+    dataUpload = models.DateField(auto_now = True, verbose_name='Data de Upload')
+
+    def __str(self):
+        return self.codigo
+
+    class Meta:
+        verbose_name_plural = 'Arquivos'
+
+
 
 # Criar a classe responsável pelo arquivo.
